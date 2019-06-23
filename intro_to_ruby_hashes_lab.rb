c9ae778cbd5e6ee3551923802f2edf854728bede
Jun Liang <illuminatemynight@gmail.com>
2019-06-23 00:26:12 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,21 @@
 def new_hash
-  new_hash = {}
+  {}
 end
 
 def my_hash
-  my_hash = {:dogs => "Fido"}
+  {:dogs => "Fido"}
 end
 
 def pioneer
-  pioneer = {:name => "Grace Hopper"}
+  {:name => "Grace Hopper"}
 end
 
 def id_generator
-  id_generator = {:id => 1}
+  {:id => 1}
 end
 
 def my_hash_creator(key, value)
-  my_hash_creator = {key => value}
+  {key => value}
 end
 
 def read_from_hash(hash, key)
@@ -25,7 +25,7 @@ end
 def update_counting_hash(hash, key)
   if hash[key]
     hash[key] += 1
-    hash 
+    hash
   else
     hash[key] = 1
     hash
