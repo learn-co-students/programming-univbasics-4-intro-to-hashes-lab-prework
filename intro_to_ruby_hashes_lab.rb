c0bf0bd19365c11fb29f6257b20e89b480831caa
Jun Liang <illuminatemynight@gmail.com>
2019-06-23 00:27:21 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,11 +23,6 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key]
-    hash[key] += 1
-    hash
-  else
-    hash[key] = 1
-    hash
-  end
+  hash[key] ? hash[key] += 1 : hash[key] = 1
+  hash 
 end
